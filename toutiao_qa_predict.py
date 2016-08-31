1ef13121498733768d195d092dce691f377b0ee6
伽楼 <jialou.jp@alibaba-inc.com>
2016-08-31 14:24:52 +0800

    add modify

--- toutiao_qa_predict.py
+++ toutiao_qa_predict.py
@@ -12,7 +12,7 @@ import pandas
 from keras.optimizers import Adam
 from scipy.stats import rankdata
 
-from deeplearning.toutiao_qa_language_modeling.toutiao_qa_eval import Evaluator
+from toutiao_qa_eval import Evaluator
 from keras_models import EmbeddingModel, AttentionModel, ConvolutionModel
 
 random.seed(42)
@@ -102,9 +102,8 @@ class EvaluatorEval(Evaluator):
       question_id = x[0]
       answer_info = x[1].sort('predict', ascending=False)
       predict = [answer_info['predict'][x] * answer_info['answer_flag'][x]
-                 for x
-                 in answer_info.index]
-      from deeplearning.toutiao_qa_language_modeling.ndcg import ndcg_at_k
+                 for x in answer_info.index]
+      from ndcg import ndcg_at_k
       scores.append(ndcg_at_k(predict, 5) * 0.5 + ndcg_at_k(predict, 10) * 0.5)
 
     print('ndcg mean is %lf' % np.mean(scores))
@@ -114,8 +113,15 @@ class EvaluatorEval(Evaluator):
 if __name__ == '__main__':
   import numpy as np
 
-  model_dir = '2016-08-30 16:18:23'
-  sim_type = 'gesd'
+  model_dir = '.'  # 0.02aesd,epoch4 local total 0.670610
+                   #
+  # model_dir = '2016-08-30 16:18:23'  #0.02gesd,epoch4 local total 0.646764
+  # model_dir = '2016-08-30 16:43:19'  #0.1gesd,epoch4 local total 0.612545
+  # model_dir = '2016-08-30 17:08:44'  #0.5gesd,epoch4 local total 0.606227
+  # model_dir = '2016-08-30 18:01:43'  #0.5aesd,epoch4 local total 0.590244
+  # sim_type = 'gesd'
+  sim_type = 'aesd'
+  epoch = 5
 
   conf = {
     'question_len': 50,
@@ -183,9 +189,9 @@ if __name__ == '__main__':
   # np.save(open('models/embedding_1000_dim.h5', 'wb'), weights)
 
   # train the model
-  evaluator.load_epoch(model, 1)
-  # output = evaluator.valid(model)
-  output = evaluator.predict(model)
+  evaluator.load_epoch(model, epoch)
+  output = evaluator.valid(model)
+  # output = evaluator.predict(model)
 
   # print(output)
 
