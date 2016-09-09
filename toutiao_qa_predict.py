c65a7dc320a9c0ba5ccc8e2314f19a43439230b2
伽楼 <jialou.jp@alibaba-inc.com>
2016-09-09 16:51:43 +0800

    modify to sim type

--- toutiao_qa_predict.py
+++ toutiao_qa_predict.py
@@ -120,14 +120,16 @@ if __name__ == '__main__':
   # model_dir = '2016-08-30 16:43:19'  #0.1gesd,epoch4 local total 0.612545
   # model_dir = '2016-08-30 17:08:44'  #0.5gesd,epoch4 local total 0.606227
   # model_dir = '2016-08-30 18:01:43'  #0.5aesd,epoch4 local total 0.590244
-  model_dir = '2016-08-31 17:15:11'  #modify param 0.02aesd,epoch1 0.637881
+  # model_dir = '2016-08-31 17:15:11'  #modify param 0.02aesd,epoch1 0.637881
                                      #2  0.668108
                                      #3  0.678860
                                      #4  0.68599
                                      #9  0.705385
-  # sim_type = 'gesd'
-  sim_type = 'aesd'
-  epoch = 5
+  model_dir = '2016-09-09 09:19:06'
+
+  sim_type = 'gesd'
+  # sim_type = 'aesd'
+  epoch = 1
 
   conf = {
     'question_len': 50,
@@ -174,7 +176,7 @@ if __name__ == '__main__':
 
     'similarity_params': {
       # 'mode': 'gesd',
-      'mode': 'aesd',
+      'mode': sim_type,
       'gamma': 1,
       'c': 1,
       'd': 2,
